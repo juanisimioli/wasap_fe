1bb23a9a39d909fc6f82ccf8651d550ed6819541
Juani Simioli <juanisimioli@gmail.com>
2023-12-19 20:21:11 -0300

    add sepolia smart contract to check live

--- config.js
+++ config.js
@@ -10,5 +10,5 @@ export const wasapContractAddress = {
   // LOCALHOST deployed contract
   31337: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
   // SEPOLIA deployed contract
-  11155111: "0x0cC39158A76F176F02FB7A025D3394B7e0A6cCd3",
+  11155111: "0x5fE521E62B90592b93E7bd8cC33939668b766CF2",
 };
